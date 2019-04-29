6cdd5ed6369810aaafc7d803b3419416425ce02f
Brandon Cooke <bcooke@resicap.com>
2019-04-29 16:34:19 -0400

    added mocha and chai with gulp command to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
+const path = require('path')
 const gulp = require('gulp')
-const tsc = require('gulp-typescript')
+const ts = require('gulp-typescript')
+const mocha = require('gulp-mocha')
 
 const tsProd = ts.createProject('tsconfig.json')
+const JS_DIR = path.join(__dirname, 'dist')
+const TS_DIR = path.join(__dirname, 'src')
+const TEST_DIR = path.join(JS_DIR, 'test')
 
 function tsc(){
 	return gulp.src(TS_DIR + '/**/*.ts')
 		.pipe(tsProd())
 		.pipe(gulp.dest(JS_DIR))
+}
+
+function test(){
+	return gulp.src(TEST_DIR + '/**/*.test.js')
+		.pipe(mocha())
 }
 
 function start(cb){
@@ -23,4 +33,5 @@ function start(cb){
 }
 
 exports.tsc = tsc
+exports.test = gulp.series(tsc, test)
 exports.start = gulp.series(tsc, start)
